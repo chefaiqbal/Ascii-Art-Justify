40aa3babaf33fb895121f8148d8eba3e5e0afbcd
tnji <tnji@noreply.learn.reboot01.com>
2023-09-25 14:14:17 +0000

    Update 'main.go'

--- main.go
+++ main.go
@@ -2,181 +2,173 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
-	"log"
 	"os"
-	"os/exec"
-	"strconv"
 	"strings"
+	"syscall"
+	"unicode"
+	"unsafe"
 )
 
-var termWidth int
-
-func init() {
-	termWidth = getTerminalWidth()
-}
-
 func main() {
-	if len(os.Args) < 2 || !isValidFormat(os.Args) {
-		printUsage()
-		return
-	}
+	var alignment, bannerType string
+	flag.StringVar(&alignment, "align", "left", "text alignment (left, center, right, justify)")
+	flag.StringVar(&bannerType, "type", "standard", "banner type (standard, shadow, tinkertoy)")
 
-	font := "standard.txt"
-	align := "left"
-	text := ""
+	err := "\033[47mUsage: go run . [OPTION] [STRING] [BANNER] \n\nExample: go run . --align=right something standard\n\033[0m"
 
-	// Parse command-line arguments
-	for _, v := range os.Args[1:] {
-		if strings.HasPrefix(v, "--align=") {
-			align = v[8:]
-		} else if v == "standard" || v == "shadow" || v == "thinkertoy" {
-			font = v + ".txt"
-		} else {
-			text += v + " "
-		}
-	}
-
-	text = strings.TrimSpace(text)
-
-	switch align {
-	case "left", "right", "center", "centre":
-		renderText(text, font, align)
-	case "justify":
-		justifyText(text, font)
-	default:
-		printUsage()
-	}
-}
-
-func isValidFormat(args []string) bool {
-	if len(args) < 2 {
-		return false
-	}
-
-	// Check if the first argument is "--align=<type>"
-	if !strings.HasPrefix(args[1], "--align=") {
-		return false
-	}
-
-	// Check if there is at least one more argument (STRING or BANNER)
-	if len(args) < 3 {
-		return false
-	}
-
-	return true
-}
-
-func renderText(text, font, align string) {
-	res := ""
-	args := strings.Split(text, "\\n")
-	for _, word := range args {
-		for i := 0; i < 8; i++ {
-			for _, letter := range word {
-				res += getLine(1+int(letter-' ')*9+i, font)
-			}
-			printAlignedText(res, align)
-			res = ""
-		}
-	}
-}
-
-func justifyText(words, font string) {
-	lines := strings.Split(words, "\\n")
-	for _, line := range lines {
-		words := strings.Fields(line) // Split the line into words
-		totalWidth := 0
-		for _, word := range words {
-			totalWidth += len(word)
-		}
-
-		numWords := len(words)
-		if numWords <= 1 {
-			// Nothing to justify if there's only one word or none
-			fmt.Println(line)
-			continue
-		}
-
-		// Calculate the number of spaces to distribute
-		extraSpaces := termWidth - totalWidth
-		spacesBetweenWords := extraSpaces / (numWords - 1)
-		extraSpaces %= numWords - 1 // Distribute any remaining spaces
-
-		// Build the justified line
-		justifiedLine := ""
-		for i, word := range words {
-			justifiedLine += word
-			if i < numWords-1 {
-				// Add spaces between words
-				justifiedLine += printSpaces(spacesBetweenWords)
-				if i < extraSpaces {
-					// Add an extra space for remaining spaces
-					justifiedLine += " "
-				}
-			}
-		}
-
-		// Print the justified line
-		fmt.Println(justifiedLine)
-	}
-}
-
-func getTerminalWidth() int {
-	out, err := exec.Command("tput", "cols").Output()
-	if err != nil {
-		log.Fatal(err)
-	}
-	width, err := strconv.Atoi(strings.TrimSpace(string(out)))
-	if err != nil {
-		log.Fatal(err)
-	}
-	return width
-}
-
-func getLine(num int, filename string) string {
-	f, e := os.Open("./fonts/" + filename)
-	if e != nil {
-		fmt.Println(e.Error())
+	if len(os.Args) < 2 {
+		fmt.Println(err)
 		os.Exit(0)
 	}
-	defer f.Close()
 
-	content := bufio.NewScanner(f)
-	lineNum := 0
-	line := ""
-	for content.Scan() {
-		if lineNum == num {
-			line = content.Text()
+	if string(os.Args[1]) == "--align" || strings.HasPrefix(string(os.Args[1]), "-align") {
+		fmt.Println(err)
+		os.Exit(0)
+	} else {
+		flag.Parse()
+	}
+
+	args := flag.Args()
+	userInput := args[0]
+	terminalWidth := terminalWidth()
+
+	if len(args) > 1 && (args[1] == "shadow" || args[1] == "tinkertoy") {
+		bannerType = args[1]
+	}
+	ascii := mapFont(bannerType)
+
+	isASCII := func(s string) bool {
+		for _, c := range s {
+			if c > unicode.MaxASCII {
+				return false
+			}
 		}
-		lineNum++
+		return true
 	}
-	return line
+
+	if !isASCII(userInput) {
+		fmt.Println("Error: input string must be within the range of ASCII characters.")
+		os.Exit(0)
+	}
+
+	words := []string{userInput}
+	if strings.Contains(words[0], "\\n") {
+		words = strings.Split(words[0], "\\n")
+	}
+
+	if alignment != "left" && alignment != "center" && alignment != "right" && alignment != "justify" {
+		fmt.Println("Error: alignment must be left, center, right, or justify.")
+		os.Exit(0)
+	}
+
+	alignment = strings.ToLower(alignment)
+	printOutput(words, ascii, terminalWidth, alignment)
 }
 
-func printAlignedText(text, align string) {
-	padding := ""
-	if align == "right" {
-		padding = printSpaces(termWidth - len(text))
-	} else if align == "center" || align == "centre" {
-		padding = printSpaces((termWidth - len(text)) / 2)
+func mapFont(fileName string) map[rune][]string {
+	file, err := os.Open("banner/" + fileName + ".txt")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
 	}
-	fmt.Println(padding + text)
+	defer file.Close()
+
+	asciiArr := parseFile(file)
+
+	var asciiStart rune = 32
+	ascii := make(map[rune][]string)
+	for i, char := range asciiArr {
+		ascii[rune(i+int(asciiStart))] = char
+	}
+
+	return ascii
 }
 
-func printSpaces(num int) string {
-	a := ""
-	for i := 1; i <= num; i++ {
-		a += " "
+func parseFile(file *os.File) [][]string {
+	fileScanner := bufio.NewScanner(file)
+	fileScanner.Split(bufio.ScanLines)
+	var asciiChar []string
+	var asciiArr [][]string
+
+	counter := 0
+	for fileScanner.Scan() {
+		if counter == 8 {
+			asciiChar = append(asciiChar, fileScanner.Text())
+			asciiArr = append(asciiArr, asciiChar)
+			asciiChar = []string{}
+			counter = 0
+			continue
+		}
+		counter++
+		asciiChar = append(asciiChar, fileScanner.Text())
 	}
-	return a
+
+	if err := fileScanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+
+	return asciiArr
 }
 
-func printUsage() {
-	fmt.Println(`Usage: go run . [OPTIONS] [TEXT]
+func terminalWidth() int {
+	var dimensions [2]uint16
 
-OPTIONS:
-  --align=<type>  Specify the text alignment (left, right, center, justify) (default "left")
-  [TEXT]          Text to be rendered as ASCII art
+	if _, _, err := syscall.Syscall(syscall.SYS_IOCTL, uintptr(syscall.Stdin), uintptr(syscall.TIOCGWINSZ), uintptr(unsafe.Pointer(&dimensions))); err != 0 {
+		fmt.Printf("error getting terminal size: %v\n", err)
+	}
 
-Example: go run . --align=right standard`)
+	return int(dimensions[1])
+}
+
+func printOutput(words []string, ascii map[rune][]string, terminalWidth int, align string) {
+	var alignment string
+	wordsPerLine := 0
+
+	for index, word := range words {
+		wordLength := 0
+
+		for _, runes := range word {
+			if runes == ' ' && align == "justify" {
+				wordsPerLine++
+			}
+			wordLength = wordLength + len(ascii[runes][4])
+		}
+
+		if wordLength > terminalWidth {
+			fmt.Println("Words don't fit in terminal.")
+			os.Exit(0)
+		}
+
+		switch align {
+		case "center":
+			alignment = strings.Repeat(" ", (terminalWidth-wordLength)/2)
+		case "right":
+			alignment = strings.Repeat(" ", terminalWidth-wordLength)
+		case "justify":
+			if wordsPerLine == 0 {
+				align = "none"
+			} else {
+				alignment = strings.Repeat(" ", (terminalWidth-wordLength)/wordsPerLine)
+			}
+		}
+
+		for i := 0; i <= 8; i++ {
+			for j, runes := range word {
+				if j == 0 && align != "justify" {
+					fmt.Print(alignment)
+				}
+				if align == "justify" && runes == ' ' {
+					fmt.Print(alignment)
+				}
+				fmt.Print(ascii[runes][i])
+			}
+			if i == 8 && index != len(words)-1 {
+				continue
+			}
+			fmt.Println()
+		}
+		wordsPerLine = 0
+	}
 }
